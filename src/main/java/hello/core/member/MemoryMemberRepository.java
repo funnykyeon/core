6c5caca7c6345a2d2861c97c53f7e0e0fe420649
kiwon kwon <funnykyeon@naver.com>
2022-05-29 19:31:35 +0900

    수정

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private
+
+    @Override
+    public void save(Member member) {
+
+    }
+
+    @Override
+    public Member findByName(Long memberId) {
+        return null;
+    }
+}
